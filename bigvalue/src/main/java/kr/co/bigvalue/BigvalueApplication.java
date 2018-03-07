31a4586882a9a96646b1cd5700f5c032ab29c682
kndoll <kndoll@DESKTOP-FNQIMLD>
2018-03-07 10:30:01 +0900

    init project

---
+++ bigvalue/src/main/java/kr/co/bigvalue/BigvalueApplication.java
@@ -0,0 +1,12 @@
+package kr.co.bigvalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigvalueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigvalueApplication.class, args);
+	}
+}
